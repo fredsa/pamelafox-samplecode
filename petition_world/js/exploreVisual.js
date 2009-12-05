f8ad22df564c26832b09da414afc27f6f3e90d6f
Jared.Wyles <Jared.Wyles@2702f8ff-8352-0410-9d95-ff502990257d>
2009-12-05 03:49:27 +0000

    does a check to see what map we need to load, behaviour is starting to duplicate more and more, a mapHandler class would be rather helpful if this keeps happening

--- petition_world/js/exploreVisual.js
+++ petition_world/js/exploreVisual.js
@@ -1,5 +1,10 @@
 jQuery(document).ready(function() {
+        var map = "explore_map";
+       if (location.pathname.toLowerCase().indexOf("vote") != -1){
+          map = "vote_map"
+       }
+       
         var language = 'en_us';
-        var votemap = VOTEMAP.initialize("explore_map", new GLatLng(55.6763, 12.5681), G_HYBRID_MAP, language);
+        var votemap = VOTEMAP.initialize(map, new GLatLng(55.6763, 12.5681), G_HYBRID_MAP, language);
         VOTEMAP.startScan();
 });
