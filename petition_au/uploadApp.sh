a21108e63333e87cfc4e6db8d8745f7f115ba4a4
pamela.fox <pamela.fox@2702f8ff-8352-0410-9d95-ff502990257d>
2009-12-30 11:56:15 +0000

    Removing unneeded files

--- petition_au/uploadApp.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-#
-# Copyright 2008 Google Inc. All Rights Reserved.
-# Author: pamelafox@google.com (Pamela Fox)
-../../google_appengine/appcfg.py --email=pamela.fox@gmail.com update .
