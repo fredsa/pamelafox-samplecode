acd39e17fb0a5222d5657ce198f1c44e8780a5c9
pamela.fox <pamela.fox@2702f8ff-8352-0410-9d95-ff502990257d>
2010-08-30 23:31:54 +0000

    Adding caching and comments.

--- posterousjs/api.py
+++ posterousjs/api.py
@@ -10,36 +10,32 @@ from xml.dom import minidom
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp.util import run_wsgi_app
 from google.appengine.api import urlfetch
+from google.appengine.api import memcache
+
 from django.utils import simplejson
 
 class ApiHandler(webapp.RequestHandler):
+  """ Handles all calls to api/, and responds with JSONinified version of
+  equivalent Posterous API call."""
 
   def get(self):
-    path = self.request.path
-    query_string = self.request.query_string
-    url = 'http://posterous.com' + path + '?' + query_string
-    result = urlfetch.fetch(url, deadline=10)
-    logging.info(result.status_code)
     dict = None
-    if result.status_code == 200:
-      dom = minidom.parseString(result.content)
-      errors = dom.getElementsByTagName('err')
-      logging.info(errors)
-      if errors:
-        dict = {'error': errors[0].getAttribute('msg')}
-      # Top-level will either be posts, tags, or sites
-      elif path.find('readposts') > -1:
-        dict = self.flat_dict(dom, 'post')
-      elif path.find('gettags') > -1:
-        dict = self.flat_dict(dom, 'tag')
-      elif path.find('getsites') > -1:
-        dict = self.flat_dict(dom, 'site')
-    else:
-      dict = {'error': '404'}
 
+    # Form URL to Posterous API according to request URL
+    url = 'http://posterous.com' + self.request.path + '?' + self.request.query_string
+
+    # Check if the result is cached
+    cached_result = memcache.get(url)
+    if cached_result:
+      dict = simplejson.loads(cached_result)
+    else:
+      dict = self.convert_results(url)
+
+    # Output JSON result, and wrap in callback if provided
     if dict:
-      logging.info(dict)
       json = simplejson.dumps(dict)
+      # Stick it in cache for 5 minutes
+      memcache.set(url, json, 300)
       callback = self.request.get('callback')
       self.response.headers['Content-Type'] = 'application/json'
       if callback:
@@ -47,7 +43,33 @@ class ApiHandler(webapp.RequestHandler):
       else:
         self.response.out.write(json)
 
-  def flat_dict(self, dom, tag_name):
+  def convert_results(self, url):
+    """ Returns a Python dict containing the API results for given URL."""
+
+    dict = None
+
+    # Fetch the URL (with 10 seconds window, in case API is slow to respond)
+    result = urlfetch.fetch(url, deadline=10)
+    if result.status_code == 200:
+      dom = minidom.parseString(result.content)
+      # Check for Posterous API errors first
+      errors = dom.getElementsByTagName('err')
+      if errors:
+        dict = {'error': errors[0].getAttribute('msg')}
+      # Top-level will either be posts, tags, or sites
+      elif url.find('readposts') > -1:
+        dict = self.convert_dom(dom, 'post')
+      elif url.find('gettags') > -1:
+        dict = self.convert_dom(dom, 'tag')
+      elif url.find('getsites') > -1:
+        dict = self.convert_dom(dom, 'site')
+    else:
+      dict = {'error': result.status_code}
+    return dict
+
+  def convert_dom(self, dom, tag_name):
+    """ Returns a Python dict for the top-level tags in a given API result."""
+
     dict = {}
     top_nodes = dom.getElementsByTagName(tag_name)
     nodes_list = []
